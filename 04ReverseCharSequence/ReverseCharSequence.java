9e9761264cef24fb34bf8bb1b934a14415ee3f6e
jrubin13 <jrubin@stuy.edu>
2017-11-24 15:46:56 -0500

    lab completed

--- 04ReverseCharSequence/ReverseCharSequence.java
+++ 04ReverseCharSequence/ReverseCharSequence.java
@@ -1,12 +1,45 @@
 public class ReverseCharSequence implements CharSequence{
-    private str = "";
+    private String str = "";
     public ReverseCharSequence(String a) {
-	for (int i = a.length()-1; i >= 0; a--) {
+	for (int i = a.length()-1; i >= 0; i--) {
 	    str += a.charAt(i);
 	}
     }
-    public int CharAt(int index) {}
+    public char charAt(int index) {
+	for (int i = 0; i < str.length(); i++) {
+	    if (index == i) {
+		return str.charAt(i);
+	    }
+	}
+	return str.charAt(0);
+    }
     
-    public int length() {}
+    public int length() {
+	int len = 0;
+	int i = 0;
+	String s = "";
+	while (!(s.equals(str)))  {
+	    len += 1;
+	    s += str.charAt(i);
+	    i += 1;
+	}
+	return len;
+    }
     
-    public String subSequence(int start, int end) {}
+    public String subSequence(int start, int end) {
+	String ans = "";
+	for (int i = start; i <= end; i++) {
+	    if (i >= str.length()) {
+		return ans;
+	    }
+	    ans += str.charAt(i);
+	}
+	return ans;
+    }
+    public static void main(String[] args) {
+	ReverseCharSequence ans = new ReverseCharSequence("ymerej");
+	System.out.println(ans.length());
+	System.out.println(ans.charAt(3));
+	System.out.println(ans.subSequence(0,8));
+    }
+}
---
+++ 04ReverseCharSequence/ReverseCharSequence.java~
@@ -0,0 +1,12 @@
+public class ReverseCharSequence implements CharSequence{
+    private str = "";
+    public ReverseCharSequence(String a) {
+	for (int i = a.length()-1; i >= 0; a--) {
+	    str += a.charAt(i);
+	}
+    }
+    public int CharAt(int index) {}
+    
+    public int length() {}
+    
+    public String subSequence(int start, int end) {}
