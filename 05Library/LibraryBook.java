427cf7b4c3b1c56aadc1dc5bd1b88a11694b3659
jrubin13 <jrubin@stuy.edu>
2017-11-28 14:47:25 -0500

    working on librarybooK

--- 05Library/LibraryBook.java
+++ 05Library/LibraryBook.java
@@ -1,4 +1,12 @@
 abstract class LibraryBook extends Book implements Comparable<LibraryBook> {
     private String callNumber;
 
-    public LibraryBook(String author, String title, String ISBN, String callNum)
+    public LibraryBook(String author, String title, String ISBN, String callNum) {
+	super(author, title, ISBN);
+	callNumber = callNum;
+    }
+    public String getAuthor() {
+	return super(getAuthor());
+    }
+    public void setAuthor(String writer) {
+	
---
+++ 05Library/LibraryBook.java~
@@ -0,0 +1,4 @@
+abstract class LibraryBook extends Book implements Comparable<LibraryBook> {
+    private String callNumber;
+
+    public LibraryBook(String author, String title, String ISBN, String callNum)
