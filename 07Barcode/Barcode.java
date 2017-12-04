14cb6c9535a2af8631603fb371522b3d1725f520
jrubin13 <jrubin@stuy.edu>
2017-12-03 22:26:06 -0500

    started barcode

---
+++ 07Barcode/Barcode.java
@@ -0,0 +1,46 @@
+public class Barcode {
+    private String data;
+    private String[] converter = new String[10];
+    
+    public Barcode(String nums) {
+	data = nums;
+	converter[0] = "||:::";
+        converter[1] = ":::||";
+        converter[2] = "::|:|";
+        converter[3] = "::||:";
+        converter[4] = ":|::|";
+	converter[5] = ":|:|:";
+	converter[6] = ":||::";
+	converter[7] = "|:::|";
+	converter[8] = "|::|:";
+	converter[9] = "|:|::";
+    }
+    public String getZip() {
+	return data;
+    }
+    public String getCode() {
+	return "|" + numToCode() + "|";
+    }
+    public boolean equals(Barcode other) {
+	if (data.equals(other.data)) {
+	    return true;
+	}
+	return false;
+    }
+    public int compareTo(Barcode other) {
+	return data.compareTo(other.data);
+    }
+    public String numToCode() {
+	String ans = "";
+	int value = 0;
+	for (int i = 0; i < data.length(); i++) {
+	    value = i;
+	    for (int x = 0; x < converter.length; x++) {
+		if (x == value) {
+		    ans += converter[x];
+		}
+	    }
+	}
+	return ans;
+    }
+}
